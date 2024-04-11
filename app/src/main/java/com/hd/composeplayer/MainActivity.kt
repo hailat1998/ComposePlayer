e12651a623e97a533a12b0017cf940e0b47a0267
hailat1998 <htemesgen400@gmail.com>
2024-04-11 08:05:22 +0000

    added the player

--- app/src/main/java/com/hd/composeplayer/MainActivity.kt
+++ app/src/main/java/com/hd/composeplayer/MainActivity.kt
@@ -3,14 +3,17 @@ package com.hd.composeplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.hd.composeplayer.ui.theme.ComposePlayerTheme
+import com.hd.composevideoPlayer.videoPlayer.Player
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +25,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposePlayerTheme {
-        Greeting("Android")
+fun HomeScreen(){
+    Box(Modifier.fillMaxSize()){
+        val context = LocalContext.current
+        val assetsVid = context.assets.list("")?.get(0)
+        Player(context = context , url = assetsVid!! , selected = true)
     }
 }
+
+
--- app/src/main/java/com/hd/composeplayer/player/player.kt
+++ app/src/main/java/com/hd/composeplayer/player/player.kt
@@ -1,29 +1,29 @@
-package com.hd.composeplayer.player
+package com.hd.composevideoPlayer.videoPlayer
 
 import android.content.Context
 import android.net.Uri
 import androidx.annotation.OptIn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultDataSourceFactory
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
 
-@OptIn(UnstableApi::class) @Composable
+@OptIn(UnstableApi::class)
+@Composable
 fun Player(context: Context, url: String, selected: Boolean){
-val player = remember{
+    val videoPlayer = remember{
     ExoPlayer.Builder(context)
         .build().apply {
             val mediaSource = ProgressiveMediaSource
                 .Factory(
-                    DefaultDataSourceFactory(context, "composePlayer")
+                    DefaultDataSourceFactory(context, null)
                 )
                 .createMediaSource(MediaItem.fromUri(Uri.parse("asset:///${url}")))
 
@@ -31,19 +31,25 @@ val player = remember{
             this.prepare()
         }
 }
-
+    videoPlayer.repeatMode = ExoPlayer.REPEAT_MODE_OFF
+    AndroidView({
+        PlayerView(it).apply {
+            useController = true
+            player = videoPlayer
+            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+        }
+    }
+    )
+    videoPlayer.playWhenReady = true
+    DisposableEffect(key1 = true) {
+        onDispose {
+            videoPlayer.release()
+        }
+    }
 }
 
 
-fun initializePlayer(context: Context) {
-    ExoPlayer.Builder(context)
-        .build()
-}
 
 
-@Composable
-private fun hideSystemUi() {
-    val systemUiController = SystemUiController(window)
-    systemUiController.hideSystemBars()
-}
+
 
